72cd7b0cc4546eebbe3341914d17539799e26a4e
Natalie Underberg-Goode <nunderbe@ucf.edu>
2019-09-18 19:42:43 -0400

    all through the town

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#%%
+import random
+#%%
+verbs = ["run", "jump", "plays"]
+print(verbs)
+
+#%%
+verbs = ["exclaims", "cross stitches", "eats ham"]
+print("The boy " + random.choice(verbs) + " all day long")
+
+#%%
