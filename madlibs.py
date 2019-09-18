141e3f776bc254aa9b0fc9b29b3716fd26824b15
Natalie Underberg-Goode <nunderbe@ucf.edu>
2019-09-18 19:56:18 -0400

    Pete is cool

--- madlibs.py
+++ madlibs.py
@@ -6,6 +6,6 @@ print(verbs)
 
 #%%
 verbs = ["exclaims", "cross stitches", "eats ham"]
-print("The boy " + random.choice(verbs) + " says Pete the cat who loves his white shoes")
+print("The boy " + random.choice(verbs) + " says Pete the cat who loves his white shoes because he is cool")
 
 #%%
